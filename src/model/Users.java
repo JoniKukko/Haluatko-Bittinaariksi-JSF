c474e5db83ef73e2854c6030361a436d3501a0d3
valje <omaposti666@gmail.com>
2016-04-02 11:39:33 +0300

    User and users class bases.

---
+++ src/model/Users.java
@@ -0,0 +1,35 @@
+package model;
+
+import java.util.List;
+
+/**
+ * Created by valje on 2.4.2016.
+ */
+
+public class Users {
+    private List<User> users;
+
+
+
+    public Users(List<User> users) {
+        this.users = users;
+    }
+
+
+
+    public List<User> getUsers() {
+        return users;
+    }
+
+
+
+    public void setUsers(List<User> users) {
+        this.users = users;
+    }
+
+
+
+    public void addUser(User user) {
+        this.users.add(user);
+    }
+}
